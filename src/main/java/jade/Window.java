1e651d100d45bb8afdb73221d395046b5f599cc9
glfos <glfoster@loyola.edu>
2024-01-30 17:16:19 -0500

    Created mouse and key listeners

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -15,12 +15,19 @@ public class Window {
     private String title;
     private long glfwWindow;
 
+    private float r,g,b,a;
+    private boolean fadeToBlack = false;
+
     private static Window window = null;
 
     private Window(){
         this.width = 1920;
         this.height = 1080;
         this.title = "Mario";
+        r = 1;
+        b = 1;
+        g = 1;
+        a = 1;
 
     }
 
@@ -68,6 +75,11 @@ public class Window {
             throw new IllegalStateException("Failed to create the new GLFW window");
         }
 
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
         // Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
         // Enable v-sync
@@ -89,8 +101,19 @@ public class Window {
             // Poll events
             glfwPollEvents();
 
-            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            if(fadeToBlack){
+                r = Math.max(r - 0.01f, 0);
+                g = Math.max(g - 0.01f, 0);
+                b = Math.max(b - 0.01f, 0);
+
+            }
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)){
+                fadeToBlack = true;
+            }
 
             glfwSwapBuffers(glfwWindow);
         }
