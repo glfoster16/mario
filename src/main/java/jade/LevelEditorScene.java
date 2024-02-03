a60bd38f346cb444e4fe7350c4accc177f39d359
glfos <glfoster@loyola.edu>
2024-02-03 13:54:03 -0500

    Created Texture class to allow texture loading using LWJGL3

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -3,12 +3,14 @@ package jade;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
+import renderer.Texture;
 import util.Time;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
 import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL20.glVertexAttribPointer;
 import static org.lwjgl.opengl.GL30.glBindVertexArray;
 import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
@@ -16,11 +18,11 @@ public class LevelEditorScene extends Scene{
     private int vertexID, fragmentID, shaderProgram;
 
     private float[] vertexArray = {
-        // position                 // color
-        100.5f, 0.5f, 0.0f,          1.0f, 0.0f, 0.0f, 1.0f, // Bottom right
-        0.5f, 100.5f, 0.0f,          0.0f, 1.0f, 0.0f, 1.0f, // Top left
-        100.5f, 100.5f, 0.0f,           0.0f, 0.0f, 1.0f, 1.0f, // Top right
-        0.5f, 0.5f, 0.0f,         1.0f, 1.0f, 0.0f, 1.0f, // Bottom left
+        // position                 // color                // UV Coordinates
+        100f,   0f, 0.0f,          1.0f, 0.0f, 0.0f, 1.0f,   1, 1,  // Bottom right
+          0f, 100f, 0.0f,          0.0f, 1.0f, 0.0f, 1.0f,   0, 0,  // Top left
+        100f, 100f, 0.0f,          0.0f, 0.0f, 1.0f, 1.0f,   1, 0,  // Top right
+          0f,   0f, 0.0f,          1.0f, 1.0f, 0.0f, 1.0f,   0, 1,  // Bottom left
     };
 
     // IMPORTANT: Must be in counter-clockwise order
@@ -39,7 +41,7 @@ public class LevelEditorScene extends Scene{
     private int vaoID, vboID, eboID;
 
     private Shader defaultShader;
-
+    private Texture testTexture;
     public LevelEditorScene(){
 
     }
@@ -49,6 +51,7 @@ public class LevelEditorScene extends Scene{
         this.camera = new Camera(new Vector2f());
         defaultShader = new Shader("assets/shaders/default.glsl");
         defaultShader.compileAndLink();
+        this.testTexture = new Texture("assets/images/testImage.png");
 
 
         // =============================================================
@@ -77,19 +80,27 @@ public class LevelEditorScene extends Scene{
         // Add the vertex attribute pointers
         int positionsSize = 3;
         int colorSize = 4;
-        int floatSizeBytes = 4;
-        int vertexSizeBytes = (positionsSize + colorSize) * floatSizeBytes;
+        int uvSize = 2;
+        int vertexSizeBytes = (positionsSize + colorSize + uvSize) * Float.BYTES;
         glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeBytes, 0);
         glEnableVertexAttribArray(0);
 
-        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * floatSizeBytes);
+        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * Float.BYTES);
         glEnableVertexAttribArray(1);
 
+        glVertexAttribPointer(2, uvSize, GL_FLOAT, false, vertexSizeBytes, (positionsSize + colorSize) * Float.BYTES);
+        glEnableVertexAttribArray(2);
     }
 
     @Override
     public void update(float dt) {
         defaultShader.use();
+
+        // Upload texture to shader
+        defaultShader.uploadTexture("TEX_SAMPLER", 0);
+        glActiveTexture(GL_TEXTURE0);
+        testTexture.bind();
+
         defaultShader.uploadMat4f("uProjection", camera.getProjectionMatrix());
         defaultShader.uploadMat4f("uView", camera.getViewMatrix());
         defaultShader.uploadFloat("uTime", Time.getTime());
