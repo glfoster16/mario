6068b85692e5a2ba2e1efa903545b1f9ae259813
glfos <glfoster@loyola.edu>
2024-02-09 12:55:04 -0500

    Created the Entity Component System - will start using effectively soon

--- src/main/java/jade/LevelEditorScene.java
+++ src/main/java/jade/LevelEditorScene.java
@@ -1,5 +1,7 @@
 package jade;
 
+import components.FontRenderer;
+import components.SpriteRenderer;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
@@ -42,12 +44,21 @@ public class LevelEditorScene extends Scene{
 
     private Shader defaultShader;
     private Texture testTexture;
+
+    GameObject testObj;
+    private boolean firstTime = false;
     public LevelEditorScene(){
 
     }
 
     @Override
     public void init(){
+        System.out.println("Creating 'test object'");
+        this.testObj = new GameObject("test object");
+        this.testObj.addComponent(new SpriteRenderer());
+        this.testObj.addComponent(new FontRenderer());
+        this.addGameObjectToScene(this.testObj);
+
         this.camera = new Camera(new Vector2f());
         defaultShader = new Shader("assets/shaders/default.glsl");
         defaultShader.compileAndLink();
@@ -120,5 +131,18 @@ public class LevelEditorScene extends Scene{
         glBindVertexArray(0);
 
         defaultShader.detach();
+
+        if (!firstTime) {
+            System.out.println("Creating gameObject!");
+            GameObject go = new GameObject("Game Test 2");
+            go.addComponent(new SpriteRenderer());
+            this.addGameObjectToScene(go);
+            firstTime = true;
+        }
+
+
+        for (GameObject go : this.gameObjects){
+            go.update(dt);
+        }
     }
 }
