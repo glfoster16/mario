bcdce0294f181f18cb61f5bf63c459a6049f1007
glfos <glfoster@loyola.edu>
2024-02-13 18:32:29 -0500

    Added batch rendering in LWJGL 3 to improve FPS by having one big draw call (a batch) instead of 1000 tiny draw calls

--- src/main/java/jade/Scene.java
+++ src/main/java/jade/Scene.java
@@ -1,10 +1,13 @@
 package jade;
 
+import renderer.Renderer;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Scene {
 
+    protected Renderer renderer = new Renderer();
     protected Camera camera;
     private boolean isRunning = false;
     protected List<GameObject> gameObjects = new ArrayList<>();
@@ -21,6 +24,7 @@ public abstract class Scene {
     public void start(){
         for (GameObject go : gameObjects){
             go.start();
+            this.renderer.add(go);
         }
         isRunning = true;
     }
@@ -31,10 +35,15 @@ public abstract class Scene {
         } else{
             gameObjects.add(go);
             go.start();
+            this.renderer.add(go);
         }
 
     }
 
     public abstract void update(float dt);
+
+    public Camera camera(){
+        return this.camera;
+    }
 
 }
